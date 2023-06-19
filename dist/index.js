89d8b38187e06aca035ab47fa71832ceadbe30b9
matjazskulj <matjazs88@gmail.com>
2023-06-19 13:41:10 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -223,7 +223,7 @@ function main() {
 
             diff = response.data.diff_url
                 ? yield octokit
-                    .request({ url: "https://api.github.com/4thOffice/minion/compare/0aa034eed9dd71686f6015dd7ef6dc4970d299d5...8e48a02b61f5f812a81015348cf74e6904db317d.diff" })
+                    .request({ url: diff })
                     .then((res) => res.data)
                 : null;
 	    console.log("------------------------------------------------- after diff.........")
